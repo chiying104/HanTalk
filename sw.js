b985f797d9a1623e583c7a02f179eea8c701bb08
chiying104 <chiying104@gmail.com>
2026-07-31 18:46:38 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,6 @@
+'use strict';
+const CACHE='hantalk-v7-7001';
+const ASSETS=['./','./index.html','./css/style.css?v=7001','./js/data.js?v=7001','./js/voice.js?v=7001','./js/app.js?v=7001','./manifest.webmanifest'];
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
+self.addEventListener('activate',e=>e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),self.clients.claim()])));
+self.addEventListener('fetch',e=>{if(e.request.mode==='navigate'){e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(x=>x.put('./index.html',c));return r}).catch(()=>caches.match('./index.html')));return}e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request)))});
